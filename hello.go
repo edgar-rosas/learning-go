acc56a51efaa28c5009324920efc7f46b87f52f5
Edgar Rosas <me@edgarrosas.dev>
2024-08-21 16:52:05 -0700

    hello world

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func Hello() string {
+	return "Hello World"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
+
+//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
+// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
