3d1cef051369d7a97f5a1b28780724c88af6d892
Edgar Rosas <me@edgarrosas.dev>
2024-09-12 10:09:43 -0700

    maths

--- maths/clockface_acceptance_test.go
+++ maths/clockface_acceptance_test.go
@@ -1,1 +1,60 @@
 package maths
+
+import (
+	"math"
+	"testing"
+	"time"
+)
+
+func TestSecondHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(t, 0, 0, 30), Point{0, -1}},
+		{simpleTime(t, 0, 0, 45), Point{-1, 0}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(t, c.time), func(t *testing.T) {
+			got := secondHandPoint(c.time)
+			if !roughlyEqualPoint(t, got, c.point) {
+				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
+			}
+		})
+	}
+}
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
+
+func roughlyEqualFloat64(t testing.TB, a, b float64) bool {
+	t.Helper()
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoint(t testing.TB, a, b Point) bool {
+	t.Helper()
+	return roughlyEqualFloat64(t, a.X, b.X) &&
+		roughlyEqualFloat64(t, a.Y, b.Y)
+}
--- maths/clockface_test.go
+++ maths/clockface_test.go
@@ -1,1 +1,38 @@
 package maths
+
+import (
+	"math"
+	"testing"
+	"time"
+)
+
+func TestSecondsInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(t, 0, 0, 30), math.Pi},
+		{simpleTime(t, 0, 0, 0), 0},
+		{simpleTime(t, 0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(t, 0, 0, 7), (math.Pi / 30) * 7},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(t, c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+}
+
+func simpleTime(t testing.TB, hours, minutes, seconds int) time.Time {
+	t.Helper()
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(te testing.TB, t time.Time) string {
+	te.Helper()
+	return t.Format("15:04:05")
+}
